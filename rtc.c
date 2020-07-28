27122d5d5d9fe3788d72591d034b6007b9148366
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-07-28 13:35:37 -0700

    Port to freedom-metal-next API
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- rtc.c
+++ rtc.c
@@ -14,16 +14,14 @@
 
 bool caught_rtc_int = false;
 
-void rtc_handler(int id, void *data) {
-	struct metal_rtc *rtc = (struct metal_rtc *) data;
+void metal_rtc_handler(void) {
+	struct metal_rtc rtc = metal_rtc_get_device(0);
 
 	/* Stop the rtc */
 	metal_rtc_run(rtc, METAL_RTC_STOP);
 
 	/* Disable RTC interrupt */
-	struct metal_interrupt *rtc_intr = metal_rtc_get_interrupt(rtc);
-	int rtc_id = metal_rtc_get_interrupt_id(rtc);
-	metal_interrupt_disable(rtc_intr, rtc_id);
+	metal_rtc_disable_interrupt(rtc);
 
 	/* Clear the pending interrupt by setting the compare to a value
 	 * larger than the current count */
@@ -35,38 +33,7 @@ void rtc_handler(int id, void *data) {
 }
 
 int main() {
-	int rc;
-
-	/* Get CPU and RTC device handles */
-	struct metal_cpu *cpu = metal_cpu_get(metal_cpu_get_current_hartid());
-	if (!cpu) {
-		exit(1);
-	}
-
-	struct metal_rtc *rtc = metal_rtc_get_device(0);
-	if (!rtc) {
-		exit(2);
-	}
-
-	/* Initialize CPU and RTC interrupts */
-	struct metal_interrupt *cpu_intr = metal_cpu_interrupt_controller(cpu);
-	if (!cpu_intr) {
-		exit(3);
-	}
-	metal_interrupt_init(cpu_intr);
-
-	struct metal_interrupt *rtc_intr = metal_rtc_get_interrupt(rtc);
-	if (!rtc_intr) {
-		exit(4);
-	}
-	metal_interrupt_init(rtc_intr);
-	int rtc_id = metal_rtc_get_interrupt_id(rtc);
-
-	/* Register an interrupt handler for the rtc */
-	rc = metal_interrupt_register_handler(rtc_intr, rtc_id, rtc_handler, rtc);
-	if (rc != 0) {
-		exit(rc * -1);
-	}
+	struct metal_rtc rtc = metal_rtc_get_device(0);
 
 	const uint64_t rate = metal_rtc_get_rate(rtc);
 
@@ -77,14 +44,8 @@ int main() {
 	metal_rtc_set_compare(rtc, RTC_TIMEOUT_SECONDS * rate);
 
 	/* Enable interrupts */
-	rc = metal_interrupt_enable(rtc_intr, rtc_id);
-	if (rc != 0) {
-		exit(5);
-	}
-	rc = metal_interrupt_enable(cpu_intr, 0);
-	if (rc != 0) {
-		exit(6);
-	}
+	metal_rtc_enable_interrupt(rtc);
+	metal_cpu_enable_interrupts();
 
 	puts("Starting RTC\n");
 
